166a4853aa22d92a6e729913cb6f83abc55be523
victor.donoso <victor.donoso@ventajon.com>
2021-12-22 14:14:36 -0500

    updated Main file

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     
     public static void main(String[] args) {
         System.out.println("Hello world, from Java");
-        System.out.println("Webhook has been enabled");
+        System.out.println("Webhook has been enabled and updated");
     }
 
 }
